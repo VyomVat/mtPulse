7f809d35e8d12258357243af6631d4cc25d56731
VyomVat <p5v9@yahoo.com>
2018-08-30 17:05:26 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,24 +9,24 @@
     var channel = 'mchat';
     
     // Assign a random avatar in random color
-    avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
+    //avatar.className = 'face-' + ((Math.random() * 13 + 1) >>> 0) + ' color-' + ((Math.random() * 10 + 1) >>> 0);
 
     var p = PUBNUB.init({
-        subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
-        publish_key:   'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695'
+        //subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
+        //publish_key:   'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695'
+        //publish_key: 'pub-c-18917a5c-e61c-4df2-ade0-1040e52b52b8',
+        //subscribe_key: 'sub-c-68bc8e34-316e-11e5-89d5-0619f8945a4f'
+        publish_key: 'pub-c-79354e6f-cd7e-4b0d-a585-58844ae7aabf',
+        subscribe_key: 'sub-c-3deca76c-a898-11e8-ab44-96e83d2b591d'      
     });
 
     p.subscribe({
         channel  : channel,
-        callback : function(m) { 
+        callback : function(m) {
             output.innerHTML = '<p><i class="' + m.avatar + '"></i><span>' +  m.text.replace( /[<>]/ig, '' ) + '</span></p>' + output.innerHTML; 
         },
         presence: function(m){
-            if(m.occupancy > 1) {
-                presence.textContent = m.occupancy + ' people online';
-            } else {
-                presence.textContent = 'Nobody else is online';
-            }
+            presence.textContent = m.occupancy + ' user(s)';
         }
     });
 
@@ -43,6 +43,6 @@
             x : (input.value='')
         });
     }
-
+  
 
 })();
